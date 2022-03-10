077981bd96ceba49b96fee7f9cf062df9cbc4cea
root <franko722>
2022-03-10 12:45:53 -0800

    done with changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("with proper grammar, but the outcome is apiece of art")
+	printf("with proper grammar, but the outcome is apiece of art");
 		return(0);
 }
