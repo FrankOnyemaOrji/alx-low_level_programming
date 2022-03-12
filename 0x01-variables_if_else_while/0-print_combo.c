d2b5cfdf7aa301cb3b38d17eba45dc49a1d56a5d
root <franko722>
2022-03-12 06:26:23 -0800

    Task 9 done

---
+++ 0x01-variables_if_else_while/0-print_combo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point, prints all possible combinations
+ * of single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+	for (i = '0'; i <= '9; i++')
+	{
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
