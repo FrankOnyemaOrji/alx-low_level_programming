cf02bbc27156d357cfe2b5f6c96e5f924632e941
franko722 <orjifrank722@gmail.com>
2022-03-12 22:39:08 +0000

    Task 8 done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (1 = 0; i < 16; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
