411f4f68ec35244fb0cf7ba5e8a9031f16100802
franko722 <orjifrank722@gmail.com>
2022-03-14 03:16:40 +0000

    first try

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int comb;
+
+	for(comb = 1; comb <= 100; comb++)
+	{
+		putchar(combe);
+		putchar(',');
+		putchar(' ');
+
+		if (comb == 10)
+		{
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
