181a50047e8a320f6162046a6cb9fae42966fcb6
franko722 <orjifrank722@gmail.com>
2022-03-14 03:32:35 +0000

    third try

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,25 @@
 int main(void)
 {
 	int comb;
+	int combe;
 
-	for(comb = 1; comb <= 100; comb++)
+	for (comb = 0; comb <= 9; comb++)
 	{
-		putchar(comb);
-		putchar(',');
-		putchar(' ');
-
-		if (comb == 10)
-		{
-			break;
+		for ( combe = 0; combe <= 9; combe++)
+			{
+				if (comb < combe)
+				{
+					putchar(comb);
+					putchar(combe);
+					if (comb == 8 && combe == 9)
+					{
+						break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
-	}
 	putchar('\n');
 	return (0);
 }
