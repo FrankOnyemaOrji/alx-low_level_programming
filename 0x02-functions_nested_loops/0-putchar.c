db6fac189027dc8d09513bf3dab37752b5c2f7e8
root <franko722>
2022-03-15 15:24:50 -0700

    Task Zero Done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
  */
 int main(void) 
 {
-	char winners[9]  = {'_','p','u','t','c','h','a','r'};
+	char winners  = {'_','p','u','t','c','h','a','r'};
 	int i;
 
 	for (i = 0;i < 9; i++)
 	{
-		_putchar(winners[i]);
+		_putchar(winners);
 	}
 	_putchar('\n');
 	return (0);
