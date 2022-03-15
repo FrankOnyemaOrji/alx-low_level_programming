b69de2d86e460d13620d24276b395bb933a002a3
Frank <59745545+franko722@users.noreply.github.com>
2022-03-15 15:51:42 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
-#include "main.h"
-/** 
- * main - Entry point
+#include "holberton.h"
+/**
+ *main - entry point.
  *Description: program starts and ends here.
- * Return: Always 0 (suceess)
+ *Return: 0 on success.
  */
 int main(void)
 {
-	char winners[] = "_putchar";
-	int i;
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(winners[i]);
-	}
-	_putchar('\n');
-	return (0);
+char winners[8] = "_putchar";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(winners[i]);
+}
+_putchar('\n');
+return (0);
 }
