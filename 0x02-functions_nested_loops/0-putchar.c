c1172afc0b49c727a8bc231cd468a8ec062b7e3f
Frank <59745545+franko722@users.noreply.github.com>
2022-03-15 15:48:48 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /** 
  * main - Entry point
- *
+ *Description: program starts and ends here.
  * Return: Always 0 (suceess)
  */
 int main(void)
 {
 	char winners[] = "_putchar";
 	int i;
-	
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(winners[i]);
