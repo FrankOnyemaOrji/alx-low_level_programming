685ee38bb6518cc3ed44885960ccea9c8ad4c452
Frank <59745545+franko722@users.noreply.github.com>
2022-03-15 15:46:48 -0700

    Update 0-putchar.c
    betty errors corrected

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
-
 /** 
  * main - Entry point
  *
  * Return: Always 0 (suceess)
  */
-int main(void) 
+int main(void)
 {
-	char winners[8] = "_putchar";
+	char winners[] = "_putchar";
 	int i;
-
-	for (i =0; i < 8; i++)
+	
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(winners[i]);
-		
 	}
 	_putchar('\n');
 	return (0);
