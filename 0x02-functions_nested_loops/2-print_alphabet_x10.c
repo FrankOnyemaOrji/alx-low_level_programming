93b030f6ed6c0eeaf7308fdb7c11e5d33da10500
root <franko722>
2022-03-16 01:15:27 -0700

    task two done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10(void) - Print alphabet 
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i,k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (k = 'a', k <= 'z',k++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+	}
+
+}
+
